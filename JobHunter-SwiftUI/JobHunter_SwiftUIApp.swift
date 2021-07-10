56aa6a2adc23c42d7978eb1ee86a6bea25b32b8e
Nelson Tai <chiahsien@gmail.com>
2021-07-10 23:20:02 +0800

    Initial Commit

---
+++ JobHunter-SwiftUI/JobHunter_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobHunter_SwiftUIApp.swift
+//  JobHunter-SwiftUI
+//
+//  Created by Nelson on 2021/7/10.
+//
+
+import SwiftUI
+
+@main
+struct JobHunter_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
