a9910896714e99e094c711df86d1c1da756ff448
Nelson Tai <chiahsien@gmail.com>
2021-07-11 15:38:05 +0800

    Add fetchers

---
+++ JobHunter-SwiftUI/Fetcher/JobModel.swift
@@ -0,0 +1,18 @@
+//
+//  JobModel.swift
+//  JobHunter-SwiftUI
+//
+//  Created by Nelson on 2021/7/11.
+//
+
+import Foundation
+
+struct Job {
+    let title: String
+    let company: String
+    let url: URL
+    var salary: String?
+    var logo: URL?
+    var location: String?
+    var tags: [String]?
+}
